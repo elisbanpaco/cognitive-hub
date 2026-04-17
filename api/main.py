f7e37b5c746c3148a47e57f7a7980ed29925b795
elisbanpaco <paccoacchaelias@gmail.com>
2026-04-16 22:52:58 -0500

    fix: update api main for cors politics and routes connections

--- api/main.py
+++ api/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.AG import router as algoritmos_router
 
-app = FastAPI()
+app = FastAPI(title="Applied NLP Engine", version="0.0.1")
 
+origins = [
+    "http://localhost:3000",
+]
+# Configuración de CORS (Vital para que tu frontend se comunique)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(algoritmos_router, prefix="/api/v1/AG", tags=["algoritmos geneticos"])
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    return {"status": "API funcionando correctamente"}
